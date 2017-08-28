86bb931d2ad3b4a7b225354edb49861017e72b77
Marcel <marcel.szewczyk@gmail.com>
2017-08-28 13:18:23 +0200

    Update aws-signature.spec.ts

--- aws-signature.spec.ts
+++ aws-signature.spec.ts
@@ -25,15 +25,15 @@ describe('aws-signature tests', () => {
     beforeEach(() => { signingTool = new AwsSignature(); });
 
     it('should return correct signature', () => {
-    let data = prepareAwsSignatureInput();
-    let date = new Date('2017-08-20T10:00:22Z');
-    let output = signingTool.generateSignature(data, date);
-    expect(output['Content-Type']).toBe('application/json');
-    expect(output['X-Amz-Date']).toBe('20170820T100022Z');
-    expect(output['Authorization']).toBe(
-        'AWS4-HMAC-SHA256 Credential=RDFKKSLFLWKWLFLSK/20170820/eu-west-1/' +
-        'execute-api/aws4_request, SignedHeaders=content-type;host;x-amz-date,' +
-        ' Signature=cff19fa73afeb64af9aa7365a3f1564cc936b195d2b612342f2c50fe32719a80');
+        let data = prepareAwsSignatureInput();
+        let date = new Date('2017-08-20T10:00:22Z');
+        let output = signingTool.generateSignature(data, date);
+        expect(output['Content-Type']).toBe('application/json');
+        expect(output['X-Amz-Date']).toBe('20170820T100022Z');
+        expect(output['Authorization']).toBe(
+            'AWS4-HMAC-SHA256 Credential=RDFKKSLFLWKWLFLSK/20170820/eu-west-1/' +
+            'execute-api/aws4_request, SignedHeaders=content-type;host;x-amz-date,' +
+            ' Signature=cff19fa73afeb64af9aa7365a3f1564cc936b195d2b612342f2c50fe32719a80');
     });
 
     it('should return empty object when input data is null or undefined', () => {
