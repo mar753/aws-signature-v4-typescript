18541228b4b7263b80d752f585beeab52af8962a
Marcel Szewczyk <marcel.szewczyk@gmail.com>
2018-08-01 22:45:39 +0200

    Some improvements

--- aws-signature.ts
+++ aws-signature.ts
@@ -19,38 +19,61 @@ export class AwsSignature {
         if (!input) {
             return {};
         }
-        let amzDate = currentDate.toISOString().replace(/-|:|\..{3}/g, '');
-        let dateStamp = amzDate.substr(0, 8);
-
-        let canonicalHeaders =
-          'content-type:' + input.contentType + '\n' + 'host:'
-          + input.host + '\n' + 'x-amz-date:' + amzDate + '\n';
-
-        let signedHeaders = 'content-type;host;x-amz-date';
-        let payloadHash = CryptoJS.SHA256(input.requestParameters).toString();
-        let canonicalRequest =
-            input.method + '\n' + input.canonicalUri + '\n'
-            + input.canonicalQuerystring + '\n' + canonicalHeaders + '\n'
-            + signedHeaders + '\n' + payloadHash;
-
-        let algorithm = 'AWS4-HMAC-SHA256';
-        let credentialScope = dateStamp + '/' + input.region + '/' + input.service + '/' + 'aws4_request';
-        let stringToSign =
-            algorithm + '\n' +  amzDate + '\n' +  credentialScope +
-            '\n' +  CryptoJS.SHA256(canonicalRequest).toString();
-
-        let signingKey = this.getSignatureKey(
-            input.secretKey, dateStamp, input.region, input.service);
-        let signature = CryptoJS.HmacSHA256(stringToSign, signingKey).toString();
-
-        let authorizationHeader =
-            algorithm + ' ' + 'Credential=' + input.accessKey + '/'
-            + credentialScope + ', ' +  'SignedHeaders=' + signedHeaders
-            + ', ' + 'Signature=' + signature;
+        let { canonicalHeaders, dateStamp, amzDate } =
+            this.prepareCanonicalHeaders(currentDate, input);
+        let { canonicalRequest, signedHeaders } =
+            this.prepareCanonicalRequest(input, canonicalHeaders);
+        let { stringToSign, algorithm, credentialScope } =
+            this.generateStringToSign(dateStamp, input, amzDate, canonicalRequest);
+        let signature = this.signString(input, dateStamp, stringToSign);
+        let authorizationHeader = this.generateAuthorizationHeader(
+            algorithm, input, credentialScope, signedHeaders, signature);
 
         return {'Content-Type': input.contentType,
                 'X-Amz-Date': amzDate,
                 'Authorization': authorizationHeader};
+    }
+
+    private generateAuthorizationHeader(algorithm: string, input: AwsSignatureInputData,
+        credentialScope: string, signedHeaders: string, signature: any)
+    {
+        return algorithm + ' ' + 'Credential=' + input.accessKey + '/'
+            + credentialScope + ', ' + 'SignedHeaders=' + signedHeaders
+            + ', ' + 'Signature=' + signature;
+    }
+
+    private signString(input: AwsSignatureInputData, dateStamp: string, stringToSign: string) {
+        let signingKey = this.getSignatureKey(input.secretKey, dateStamp, input.region, input.service);
+        let signature = CryptoJS.HmacSHA256(stringToSign, signingKey).toString();
+        return signature;
+    }
+
+    private generateStringToSign(dateStamp: string, input: AwsSignatureInputData, amzDate: string,
+        canonicalRequest: string)
+    {
+        let algorithm = 'AWS4-HMAC-SHA256';
+        let credentialScope = dateStamp + '/' + input.region + '/'
+            + input.service + '/' + 'aws4_request';
+        let stringToSign = algorithm + '\n' + amzDate + '\n' + credentialScope +
+            '\n' + CryptoJS.SHA256(canonicalRequest).toString();
+        return { stringToSign, algorithm, credentialScope };
+    }
+
+    private prepareCanonicalRequest(input: AwsSignatureInputData, canonicalHeaders: string) {
+        let signedHeaders = 'content-type;host;x-amz-date';
+        let payloadHash = CryptoJS.SHA256(input.requestParameters).toString();
+        let canonicalRequest = input.method + '\n' + input.canonicalUri + '\n'
+            + input.canonicalQuerystring + '\n' + canonicalHeaders + '\n'
+            + signedHeaders + '\n' + payloadHash;
+        return { canonicalRequest, signedHeaders };
+    }
+
+    private prepareCanonicalHeaders(currentDate: Date, input: AwsSignatureInputData) {
+        let amzDate = currentDate.toISOString().replace(/-|:|\..{3}/g, '');
+        let dateStamp = amzDate.substr(0, 8);
+        let canonicalHeaders = 'content-type:' + input.contentType + '\n' + 'host:'
+            + input.host + '\n' + 'x-amz-date:' + amzDate + '\n';
+        return { canonicalHeaders, dateStamp, amzDate };
     }
 
     private getSignatureKey(
